e9dc3d82628a90917708886dea4de066b648515e
5ong <vhoang61@gmail.com>
2016-11-23 15:59:50 -0800

    with login

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models/user');
 
@@ -78,16 +80,55 @@ router.post('/register',function(req,res,next){
 		User.createUser(newUser, function(err, user){
 			if(err) throw err;
 			console.log(user);
-		})
+		});
 
 		//Success Message
 		req.flash('success', 'You are now registered and may log in');
 
-		res.location('./');
+		res.location('/');
 		res.redirect('/');
 	}
 
 });
 
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+	function(username, password, done){
+		User.getUserByUsername(username, function(err, user){
+			if(err) throw err;
+			if(!user){
+				console.log('Unknown User');
+				return done(null, false, {message: 'Unknown User'});
+			}
+
+			User.comparePassword(password, user.password, function(err, isMatch){
+				if(err) throw err;
+				if(isMatch){
+					return done(null, user);
+				} else {
+					console.log('Invalid Password');
+					return done(null, false, {message: 'Invalid Password'});
+				}
+
+			});
+
+		});
+	}
+));
+
+router.post('/login', passport.authenticate('local',{failureRedirect:'/user/login', failureFlash:'Invalid username or password'}), function(req, res){
+	console.log('authentication Successful');
+	req.flash('success', 'You are now logged in');
+	res.redirect('/');
+});
 
 module.exports = router;
